cf1cf54af8c3261a6a5536b4d3b74c24834e1b0d
Viktor <kontush14@gmail.com>
2022-09-27 10:47:11 +0300

    add table cards

---
+++ database/migrations/2022_09_27_074227_cards_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('card_number');
+            $table->integer('pin_code');
+            $table->string('fin_mobile_number');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
