647c5559772bded7941d2c5a0493df80773837ad
Viktor <kontush14@gmail.com>
2022-09-27 11:47:12 +0300

    add welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ATM</title>
+    <!-- Scripts -->
+    <link rel="stylesheet" href="<?php echo asset('css/style.css');?>" type="text/css">
 </head>
 <body>
-    <h1>Hello, world</h1>
+    <div class="container">
+        <div class="title_class">
+            <h1>Hello, world</h1>
+        </div>
+        <div class="menu_atm">
+            <h3>Withdrawal of cash using a card</h3>
+            <h3>Withdrawal of cash without a card (additional fee)</h3>
+        </div>
+    </div>
 </body>
 </html>
