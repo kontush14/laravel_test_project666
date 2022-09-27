a9b3c08fc6728d0526dca91d660bf89959a9e12a
Viktor <kontush14@gmail.com>
2022-09-27 16:07:04 +0300

    add file withdraw.php

---
+++ resources/views/withdraw.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ATM</title>
+    <!-- Scripts -->
+    <link rel="stylesheet" href="<?php echo asset('css/style.css');?>" type="text/css">
+</head>
+<body>
+    <div class="container">
+        <div class="title_class">
+            <h1>Hello, user</h1>
+        </div>
+        <div class="menu_atm">
+            <button type="button">Withdrawal of cash using a card</button>
+            <button type="button">Withdrawal of cash without a card (additional fee)</button> 
+        </div>
+    </div>
+</body>
+</html>
