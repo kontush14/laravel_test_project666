baffdb6efa2962c509682cfb866bcd188fd078ab
Viktor <kontush14@gmail.com>
2022-09-27 10:49:39 +0300

    update model Cards

--- app/Models/Cards.php
+++ app/Models/Cards.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Cards extends Model
 {
     use HasFactory;
+
+    protected $table = 'cards';
+
+    protected $fillable = [
+
+        'first_name',
+        'last_name',
+        'card_number',
+        'pin_code',
+        'fin_mobile_number',
+    ];
+
 }
